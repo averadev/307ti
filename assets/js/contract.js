47df7d2b0fcf19567b90d2964ffa1e6ed20b379a
Faustino Loeza <faustinoloezaperez@gmail.com>
2016-09-01 14:40:24 -0500

    Balance

--- assets/js/contract.js
+++ assets/js/contract.js
@@ -2155,6 +2155,11 @@ function parsearSALE(sales){
 			sales[i].Nederlands_Florins = 0;
 		}
 	}
+	for(var i = 0; i < sales. length; i++){
+		if (sales[i].Balance == 0) {
+			sales[i].Balance = '';
+		}
+	}
 	return sales;	
 }
 
